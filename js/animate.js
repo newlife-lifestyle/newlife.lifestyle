2627eb21a8ac32e1917d5c158b1862327869e02e
newlife-lifestyle <35419338+newlife-lifestyle@users.noreply.github.com>
2018-01-16 02:01:26 +0800

    Update animate.js

--- js/animate.js
+++ js/animate.js
@@ -7,19 +7,4 @@ $(document).ready(function(){
     })
 });
 
-var slideIndex = 1;
-    showImage(slideIndex);
 
-    function plusDivs(n){
-        showImage (slideIndex += n);
-    }
-    function showImage(n) {
-        var i;
-        var x = document.getElementsByClassName("comments");
-        if (n >x.length) {slideIndex=1}
-        if (n <1) {slideIndex = x.length};
-        for (i=0; i < x.length; i++) {
-            x[i].style.display="none";
-        }
-        x[slideIndex-1].style.display="block"
-    }
