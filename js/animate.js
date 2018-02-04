df137d09db97b983e583c89f7674c52dc234f4eb
newlife-lifestyle <35419338+newlife-lifestyle@users.noreply.github.com>
2018-02-05 07:45:07 +0800

    Add files via upload

--- js/animate.js
+++ js/animate.js
@@ -7,4 +7,30 @@ $(document).ready(function(){
     })
 });
 
+$(document).ready(function() {
+   $("a.active").click(function() {
+       $(this).addclass("link")
+   });
+});
 
+var slideShow = 1;
+showSlides(slideShow=1);
+
+function minusSlides(n) {
+  showSlides(slideShow += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideShow = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("customer-review");
+  if (n > slides.length) {slideShow = 1}
+  if (n < 1) {slideShow = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  };
+  slides[slideShow-1].style.display = "block";
+};
